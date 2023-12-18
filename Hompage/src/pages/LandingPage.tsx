111b3f55362d6ca7e4a137f7ad036de2e154c782
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2023-12-19 00:09:46 +0530

    Added homepage

---
+++ Hompage/src/pages/LandingPage.tsx
@@ -0,0 +1,334 @@
+import { FunctionComponent } from "react";
+import styles from "./LandingPage.module.css";
+
+const LandingPage: FunctionComponent = () => {
+  return (
+    <div className={styles.landingPage}>
+      <div className={styles.landingPageChild} />
+      <div className={styles.landingPageItem} />
+      <div className={styles.landingPageInner} />
+      <div className={styles.rectangleDiv} />
+      <div className={styles.ellipseDiv} />
+      <div className={styles.landingPageChild1} />
+      <div className={styles.landingPageChild2} />
+      <div className={styles.landingPageChild3} />
+      <div className={styles.landingPageChild4} />
+      <div className={styles.menuWrapper}>
+        <div className={styles.menu}>
+          <div className={styles.payonweb}>
+            <b className={styles.pow}>payonweb</b>
+          </div>
+          <div className={styles.faAngleDown}></div>
+          <div className={styles.faAngleDown1}></div>
+          <div className={styles.faAngleDown2}></div>
+          <b className={styles.about}>About us</b>
+          <b className={styles.resources}>Resources</b>
+          <b className={styles.industries}>Industries</b>
+          <b className={styles.products}>Products</b>
+          <div className={styles.loginParent}>
+            <div className={styles.login}>LOGIN</div>
+            <div className={styles.bookDemoWrapper}>
+              <div className={styles.bookDemo}>BOOK DEMO</div>
+            </div>
+            <div className={styles.signupWrapper}>
+              <div className={styles.bookDemo}>SIGNUP</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.manWorkingOnNftBlockchain} />
+      <div className={styles.home1Rect}>
+        <div className={styles.home1RectChild} />
+        <div className={styles.weHelpBusinesses}>
+          We help businesses to grow quickly by accepting the payment methods
+          that your customers prefer across flat and digital currencies.
+        </div>
+        <b className={styles.growYourRevenue}>
+          Grow Your Revenue with digital payment Solution
+        </b>
+        <div className={styles.swiftSimpleSecure}>Swift Simple Secure</div>
+        <div className={styles.experienceIt}>
+          <div className={styles.experienceItChild} />
+          <div className={styles.experienceIt1}>Experience it</div>
+        </div>
+      </div>
+      <img
+        className={styles.manWorkingOnNftBlockchain1}
+        alt=""
+        src="/man-working-on-nft-blockchain--1281x1191@2x.png"
+      />
+      <div className={styles.landingPageChild5} />
+      <div className={styles.landingPageChild6} />
+      <div className={styles.landingPageChild7} />
+      <div className={styles.landingPageChild8} />
+      <div className={styles.home2Rect}>
+        <div className={styles.home2RectChild} />
+        <div className={styles.convertYourFlat}>
+          Convert your flat and digital currencies in one platform instantly and
+          securely without high conversion fees in one go.
+        </div>
+        <b className={styles.multiCurrencySwapWithContainer}>
+          <p className={styles.multiCurrencySwap}>Multi-Currency Swap</p>
+          <p className={styles.multiCurrencySwap}>with low conversion fees</p>
+        </b>
+        <div className={styles.swiftEasy}>{`Swift & Easy`}</div>
+        <div className={styles.experienceIt2}>
+          <div className={styles.experienceItItem} />
+          <div className={styles.exploreMore}>Explore More</div>
+        </div>
+        <img
+          className={styles.nftWallet1280x1067}
+          alt=""
+          src="/nft-wallet--1280x1067@2x.png"
+        />
+      </div>
+      <div className={styles.trustedByBusinesses}>Trusted by Businesses</div>
+      <div className={styles.home3}>
+        <div className={styles.withPayonwebYouContainer}>
+          <span>
+            <span>With</span>
+          </span>
+          <span className={styles.span}>
+            <span>{` `}</span>
+            <span>payonweb</span>
+          </span>
+          <span>
+            <span className={styles.span}>{`, `}</span>
+            <span>
+              you are able to control everything in one single dashboard to
+              manage your account, multi-currency conversion and payments
+            </span>
+          </span>
+        </div>
+        <b className={styles.wereHereTo}>
+          We’re here to help you grow your business
+        </b>
+        <div className={styles.whatWeDo}>What we do</div>
+      </div>
+      <div className={styles.asSeenOn}>As seen on</div>
+      <div className={styles.wrapperZaobao1Parent}>
+        <div className={styles.wrapperZaobao1}>
+          <img className={styles.zaobao1Icon} alt="" src="/zaobao-1@2x.png" />
+        </div>
+        <div className={styles.wrapperPitstopTyres1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/pitstoptyres-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperMandalaClub1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/mandalaclub-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperForbes1}>
+          <img className={styles.zaobao1Icon} alt="" src="/forbes-1@2x.png" />
+        </div>
+        <div className={styles.wrapperTheBusinessTimes1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/thebusinesstimes-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperMoneyFm1}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+        <div className={styles.wrapperLumin2}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+        <div className={styles.wrapperFrame221}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+        <div className={styles.wrapperImgV22c7674ff188b4}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+      </div>
+      <div className={styles.rectangleParent}>
+        <div className={styles.frameChild} />
+        <div className={styles.socialmedia}>
+          <div className={styles.item}>
+            <img className={styles.vectorIcon} alt="" src="/vector@2x.png" />
+          </div>
+          <div className={styles.item}>
+            <img className={styles.vectorIcon} alt="" src="/vector@2x.png" />
+          </div>
+          <div className={styles.item2}>
+            <img className={styles.vectorIcon2} alt="" src="/vector@2x.png" />
+          </div>
+          <div className={styles.item3}>
+            <img
+              className={styles.itemChild}
+              alt=""
+              src="/group-32933@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frameParent}>
+          <div className={styles.requestDemoParent}>
+            <b className={styles.requestDemo}>Request Demo</b>
+            <b className={styles.requestDemo}>Contact Us</b>
+            <b className={styles.aboutUs}>About Us</b>
+            <div className={styles.whoAreWe}>Who are we</div>
+            <div className={styles.careers}>Careers</div>
+            <div className={styles.referral}>Referral</div>
+          </div>
+          <div className={styles.productsParent}>
+            <b className={styles.products1}>Products</b>
+            <div className={styles.multiCurrencySwap1}>Multi-Currency Swap</div>
+            <div className={styles.checkout}>Checkout</div>
+            <div className={styles.checkout}>POS+</div>
+            <div className={styles.payonwebCard}>payonweb Card</div>
+            <b className={styles.resources1}>Resources</b>
+            <div className={styles.checkout}>Case Studies</div>
+            <div className={styles.blog}>Blog</div>
+            <div className={styles.supportCenter}>Support Center</div>
+          </div>
+          <div className={styles.productsParent}>
+            <b className={styles.industries1}>Industries</b>
+            <div className={styles.checkout}>Automotive</div>
+            <div className={styles.checkout}>Healthcare</div>
+            <div
+              className={styles.travelHospitality}
+            >{`Travel & Hospitality`}</div>
+            <div className={styles.retail}>Retail</div>
+            <div className={styles.professionalServices}>
+              Professional Services
+            </div>
+          </div>
+        </div>
+        <div className={styles.groupParent}>
+          <div className={styles.rectangleGroup}>
+            <div className={styles.groupChild} />
+            <div className={styles.lowCost}>Low cost</div>
+            <div className={styles.transparentPricingAnd}>
+              Transparent pricing and we offer cheapest rate in the market to
+              help our clients to grow businesses.
+            </div>
+            <img
+              className={styles.averagePrice12Icon}
+              alt=""
+              src="/averageprice1-2@2x.png"
+            />
+          </div>
+          <div className={styles.rectangleGroup}>
+            <div className={styles.groupChild} />
+            <div className={styles.seamlessSecure}>{`Seamless & Secure`}</div>
+            <div
+              className={styles.transparentPricingAnd}
+            >{`We allow you to accept, transfer crypto and flexibility to withdraw to your wallets seamlessly and securely. `}</div>
+            <img className={styles.wallet2Icon} alt="" src="/wallet-2@2x.png" />
+          </div>
+          <div className={styles.groupDiv}>
+            <div className={styles.groupInner} />
+            <div
+              className={styles.regulatedLicensed}
+            >{`Regulated & Licensed`}</div>
+            <div className={styles.transparentPricingAnd}>
+              We are regulated and licensed digital payment services provider by
+              the Monetary Authority of Singapore (MAS)
+            </div>
+            <img
+              className={styles.wallet2Icon}
+              alt=""
+              src="/energywindow-1@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.home31}>
+          <div
+            className={styles.weOfferDigital}
+          >{`We offer digital payment solutions to help you grow your business. We sole all your payment needs in one place. Increase conversions and costumer satisfaction by offering `}</div>
+          <b className={styles.yourStrategicPartnerContainer}>
+            <p className={styles.yourStrategicPartner}>
+              Your Strategic partner
+            </p>
+            <p className={styles.inDigitalPayments}>in digital payments</p>
+          </b>
+          <div className={styles.whyPayonweb}>Why payonweb?</div>
+          <b className={styles.growYourRevenue1}>
+            Grow your revenue with digital payment solutions
+          </b>
+          <b className={styles.payonweb2}>payonweb</b>
+        </div>
+      </div>
+      <img
+        className={styles.marketPopularityOfNftStart}
+        alt=""
+        src="/market-popularity-of-nft-startup--1280x1067@2x.png"
+      />
+      <img
+        className={styles.dOnlinePaymentWithQrCodeIcon}
+        alt=""
+        src="/1989528853donlinepaymentwithqrcodeconcept-1@2x.png"
+      />
+      <div className={styles.wrapperScoots1Parent}>
+        <div className={styles.wrapperScoots1}>
+          <img className={styles.zaobao1Icon} alt="" src="/scoots-1@2x.png" />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/20230530164545-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/fintechtimes-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/buildinglegacies-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/beaute-on-aesthetics-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+        <div className={styles.wrapperTheGallery1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/thegallery-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperTheGallery1}>
+          <img
+            className={styles.zaobao1Icon}
+            alt=""
+            src="/thegallery-1@2x.png"
+          />
+        </div>
+        <div className={styles.wrapperScoots1}>
+          <img className={styles.zaobao1Icon} alt="" src="/money-fm-1@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
